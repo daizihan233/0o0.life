1311ea51231e46f1a26af778439db2d5124e4f98
daizihan233 <y066ydg5@duck.com>
2024-02-26 02:57:28 +0800

    修改一些表达

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,7 +8,7 @@
     一名初一学生<br>
     平时主学 Python，偶尔研究 C++、Kotlin 与 Vue 3<br>
     其他的可以看我的个人博客关于页 <a href="https://khbit.cn/about">KuoHuBlog</a><br>
-    这个网站只是突发奇想，现在前端挂在 <a href="https://vercel.com">Vercel</a> 上，服务端是挂在我的服务器上的<br>
+    这个网站只是突发奇想，现在是挂在我的服务器上的<br>
     后端使用 Python FastAPI，前端使用 Naive UI，一个 Vue 3 框架<br>
     这个网站是我现学现卖的，不算漂亮，见谅。<br>
     <br>
--- src/views/HelloWorld.vue
+++ src/views/HelloWorld.vue
@@ -41,9 +41,10 @@ function relax() {
         另外，这个网站是使用 <a href="https://www.naiveui.com/zh-CN/dark/components/tabs">Naive UI</a> 制作的，但本站的的开发者可能比这个 UI 还要天真……<br>
         （Tips：请区分本站的作者和 UI 的作者，这个 UI 算是我见过做好的 Vue 3 的 UI 框架了，严格说，我最喜欢的，也是个人觉得最容易上手的）
       </n-tab-pane>
-      <n-tab-pane name="chap3" tab="“他”让你来的？">
+      <n-tab-pane name="chap4" tab="“他”让你来的？">
         哦…… 原来是<router-link to="/about">他</router-link>啊，他我再熟悉他不过了，他可能只是第一次自己码完一个站点，太高兴了罢了<br>
-        他让你过来，其实你也不必阅读，不必浪费这个时间
+        他让你过来，其实你也不必阅读，不必浪费这个时间，如果你还是想看，那就继续往下看吧：<br>
+        （这里空空荡荡，等待着某个名为“开发者”的人来书写……）
       </n-tab-pane>
     </n-tabs>
   </div>
