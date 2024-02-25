d90a65b2ed6c1b97eec654fb3ef08629e4f8fe5c
daizihan233 <y066ydg5@duck.com>
2024-02-26 02:32:07 +0800

    HelloWorld 在编译时会被删除

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import {createRouter, createWebHistory} from "vue-router";
+import HelloWorld from "../views/HelloWorld.vue";
 
 const router = createRouter(
     {
@@ -6,7 +7,7 @@ const router = createRouter(
         routes: [
             { path: '/', name: 'Home', component: () => import("../views/Home.vue") },
             { path: '/about', name: 'About', component: () => import("../views/About.vue") },
-            { path: '/HelloWorld', name: 'HelloWorld', component: () => import("../views/HelloWorld.vue") },
+            { path: '/HelloWorld', name: 'HelloWorld', component: HelloWorld },
             { path: '/:pathMatch(.*)*', name: '404',component: () => import("../views/NotFound.vue") }
         ]
     }
