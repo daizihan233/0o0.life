117395c43f15f9e2a9827604626a49e7de0b1130
daizihan233 <y066ydg5@duck.com>
2024-03-01 03:06:55 +0800

    换行

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -9,7 +9,7 @@ document.title = "404 - 一片荒无人烟的地方，像是异世界"
   <Tips title="路牌 - 404 Not Found" text="……（这里的路牌一个字也没有，只有一片荒芜，这可不是个好兆头）" type="success"></Tips>
   <n-result status="404" title="404 资源不存在" description="生活总归带点荒谬">
     <template #footer>
-      ↓↓↓ 告诉“开发者” ↓↓↓
+      ↓↓↓ 告诉“开发者” ↓↓↓<br>
       mq5kjioc@duck.com
     </template>
   </n-result>
